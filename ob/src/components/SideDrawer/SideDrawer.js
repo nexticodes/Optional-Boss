e0402c692193cbff3a162c85a45fa855bd105627
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-04 11:33:51 -0800

    Add logic that toggles classNames depending on App state slice

--- ob/src/components/SideDrawer/SideDrawer.js
+++ ob/src/components/SideDrawer/SideDrawer.js
@@ -2,14 +2,22 @@ import React from 'react';
 
 import './SideDrawer.css';
 
-const SideDrawer = props => (
-    <nav className='side-drawer'>
-        <ul>
-            <li><a href='/'>Home</a></li>
-            <li><a href='/'>Members</a></li>
-            <li><a href='/'>Contact Us</a></li>
-        </ul>
-    </nav>
-);
+const SideDrawer = props => {
+    let drawerClasses = 'side-drawer';
+    if (props.show) {
+        drawerClasses = 'side-drawer open'
+    }
+
+    return (  
+        <nav className={drawerClasses}>
+            <ul>
+                <li><a href='/'>Home</a></li>
+                <li><a href='/'>Members</a></li>
+                <li><a href='/'>Contact Us</a></li>
+            </ul>
+        </nav>
+    );
+}
+  
 
 export default SideDrawer;
