d7e5e7c5f93e0cc39342abe08bb17e2fc2d45fa8
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-04 10:18:06 -0800

    Remove unnecessary code.

--- ob/src/App.js
+++ ob/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
+
+import Home from './components/Home/Home.js';
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Route exact path='/' render={(props) => <div><h1>Hello!</h1></div>}></Route>
-      </Router>
-    </div>
+    <Router>
+      <Route exact path='/' render={(props) => <Home/>}></Route>
+    </Router>
   );
 }
 
