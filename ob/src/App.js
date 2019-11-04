261a1349dfd897ab4d3c01fc3b2a58414df27b81
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-04 09:28:45 -0800

    Clear boilerplate code and add custom message to the user.

--- ob/src/App.js
+++ ob/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Route exact path='/' render={(props) => <div><h1>Hello!</h1></div>}></Route>
+      </Router>
     </div>
   );
 }
