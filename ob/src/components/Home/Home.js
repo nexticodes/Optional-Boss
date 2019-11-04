d2870cc082ea6a2196bdfcaf2690fb944425aa0c
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-04 10:20:06 -0800

    Add some splash message and Header component in Welcome page.

---
+++ ob/src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from '../Header/Header.js';
+
+const Home = (props) => {
+    return (
+        <div>
+            <Header/>
+            <main style={{marginTop: '64px'}}>
+                <h1>Welcome to the Optional Boss Portal</h1>
+            </main>
+            
+        </div>
+    )
+}
+
+export default Home;
