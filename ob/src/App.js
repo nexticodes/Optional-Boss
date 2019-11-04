93a50eb5835b09fe4aea75db809352f934c5ee26
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-04 11:16:46 -0800

    Turn to stateful component and add logic for toggling sidebar

--- ob/src/App.js
+++ ob/src/App.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-import Home from './components/Home/Home.js';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import SideDrawer from './components/SideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop';
 
-function App() {
-  return (
-    <Router>
-      <Route exact path='/' render={(props) => <Home/>}></Route>
-    </Router>
-  );
+class App extends Component {
+  state = {
+    sideDrawerOpen: false
+  }
+
+
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return {sideDrawerOpen: !prevState.sideDrawerOpen};
+    });
+  };
+
+  backdropClickHandler = () => {
+    // always close sidedrawer
+    this.setState({sideDrawerOpen: false});
+  };
+
+  render(){
+
+    let sideDrawer;
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      sideDrawer = <SideDrawer />;
+      backdrop = <Backdrop click={this.backdropClickHandler} />
+    }
+
+    return (
+      <div style={{height: '100%'}}>
+        <Header drawerClickHandler={this.drawerToggleClickHandler}/>
+        {sideDrawer}
+        {backdrop}
+        <Router>
+          <Route exact path='/' render={(props) => <Home/>}></Route>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
