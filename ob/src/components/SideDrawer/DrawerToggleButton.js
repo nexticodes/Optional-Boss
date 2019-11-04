8fe849b62564c15683e554296c18ebcdc29ab01b
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-04 11:18:46 -0800

    Add drawer toggle button

--- ob/src/components/SideDrawer/DrawerToggleButton.js
+++ ob/src/components/SideDrawer/DrawerToggleButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './DrawerToggleButton.css';
 
 const DrawerToggleButton = props => (
-    <button className='toggle-button'>
+    <button className='toggle-button' onClick={props.click}>
         <div className='toggle-button__line'></div>
         <div className='toggle-button__line'></div>
         <div className='toggle-button__line'></div>
