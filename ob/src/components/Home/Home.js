d7e92ef9a2aea0a47f04843356ad038d5dbbdb30
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-04 11:19:38 -0800

    Remove Header and SideDrawer components and move them to App.js

--- ob/src/components/Home/Home.js
+++ ob/src/components/Home/Home.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
-import Header from '../Header/Header.js';
-
 const Home = (props) => {
     return (
         <div>
-            <Header/>
             <main style={{marginTop: '64px'}}>
                 <h1>Welcome to the Optional Boss Portal</h1>
             </main>
-            
         </div>
     )
 }
