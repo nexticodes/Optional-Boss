61c41604700c13e337d66559c9d8465cf9ba0f82
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-05 08:26:35 -0800

    Add two images URL that will be rendered, functions that handle click on arrows, add state slice.

--- ob/src/components/Slider/Slider.js
+++ ob/src/components/Slider/Slider.js
@@ -2,11 +2,31 @@ import React, {useState} from 'react';
 
 import './Slider.css';
 
+import Slide from './Slide';
+import LeftArrow from './LeftArrow';
+import RightArrow from './RightArrow';
+
 const Slider = props => {
+
+    const [currIndex, setCurrIndex] = useState(0);
+
+    const images = [
+        'https://pbs.twimg.com/media/EF_8FwdU8AA_pTQ?format=jpg&name=400x400',
+        'https://pbs.twimg.com/media/EFbjItiUEAALV9k?format=jpg&name=400x400'
+    ]
+
+    let goPrevSlide = () => {
+    }
+
+    let goNextSlide = () => {
+    }
 
     return (
         <div className='slider'>
-            Hello :)
+            <Slide/>
+
+            <LeftArrow prevSlide={goPrevSlide}/>
+            <RightArrow nextSlide={goNextSlide}/>
         </div>
     );
 }
