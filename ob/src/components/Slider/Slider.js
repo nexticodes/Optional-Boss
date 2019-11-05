7afd5efd414f9481c59f80242812f3d9be115b3f
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-05 07:56:29 -0800

    Create Slider component.

---
+++ ob/src/components/Slider/Slider.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+import './Slider.css';
+
+const Slider = props => {
+
+    return (
+        <div className='slider'>
+            Hello :)
+        </div>
+    );
+}
+
+export default Slider;
