6f0e3dbcd17da016a61e7d976b65ea09575bc337
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-05 09:44:10 -0800

    Random commit.

--- ob/src/components/Header/Header.js
+++ ob/src/components/Header/Header.js
@@ -6,7 +6,7 @@ import './Header.css';
 import logo from '../../assets/OB logo.jpg'
 
 const Header = props => (
-    <header className='header'>
+    <div className='header'>
         <nav className='header__navigation'>
             <div className='header__logo'>
                 <a href='/'><img src={logo} alt="Optional Boss Logo" height="50px" width="50px"/></a>
@@ -23,7 +23,7 @@ const Header = props => (
                 </ul>
             </div>
         </nav>
-    </header>
+    </div>
 );
 
 export default Header;
