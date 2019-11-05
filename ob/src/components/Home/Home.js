1068958c8e6f3d143a7366a5abb28f891b04bce1
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-05 07:56:06 -0800

    Add Slider component.

--- ob/src/components/Home/Home.js
+++ ob/src/components/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Slider from '../Slider/Slider';
+
 const Home = (props) => {
     return (
         <div>
-            <main style={{marginTop: '64px'}}>
+            <main style={{marginTop: '36px'}}>
                 <h1>Welcome to the Optional Boss Portal</h1>
+                <Slider/>
             </main>
         </div>
     )
