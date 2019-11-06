04896a9c8ae91be661b6032b0733f287b58903b8
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-06 11:29:50 -0800

    Change the placement of the About component.

--- ob/src/components/Home/Home.js
+++ ob/src/components/Home/Home.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import Slider from '../Slider/Slider';
+import About from '../About/About';
 
 const Home = (props) => {
     return (
         <div>
-            <main style={{marginTop: '48px'}}>
-                <h1>Welcome to the Optional Boss Portal</h1>
+            <main style={{marginTop: '37px', marginBottom: '74px'}}>
                 <Slider/>
             </main>
+            <About/>
         </div>
     )
 }
