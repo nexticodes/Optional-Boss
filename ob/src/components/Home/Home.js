e4def33a94c27312d0dd655222f0afa24abddfbf
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-06 09:13:34 -0800

    Change margin top size to 48 px.

--- ob/src/components/Home/Home.js
+++ ob/src/components/Home/Home.js
@@ -5,7 +5,7 @@ import Slider from '../Slider/Slider';
 const Home = (props) => {
     return (
         <div>
-            <main style={{marginTop: '36px'}}>
+            <main style={{marginTop: '48px'}}>
                 <h1>Welcome to the Optional Boss Portal</h1>
                 <Slider/>
             </main>
