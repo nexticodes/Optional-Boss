afce25136bbc1025deec72c9c0ff0f94ede0bced
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-08 14:51:09 -0800

    Create folder and file for Members page.

---
+++ ob/src/components/Members/Members.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Members = (props) => {
+    return (
+        <div>
+            Members and shit go here.
+        </div>
+    );
+};
+
+export default Members;
