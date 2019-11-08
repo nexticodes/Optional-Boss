6b3b624a9c92aec61d1fd0a0faa635bd33b788ad
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-08 15:33:33 -0800

    Successfully export members data and render the names.

--- ob/src/components/Members/Members.js
+++ ob/src/components/Members/Members.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import {members} from '../../members';
+
 const Members = (props) => {
     return (
         <div>
-            Members and shit go here.
+            <main style={{marginTop: '70px', marginBottom: '74px'}}>
+                <h1>MEMBERS</h1>
+                <hr/>
+                {members.map(e => <h4>{e.name}</h4>)}
+            </main>
         </div>
     );
 };
