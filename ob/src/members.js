17172e659c1ff7101c1edccd7d15e6bbd1d106e6
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-08 10:23:08 -0800

    Create JSON file for members info.

---
+++ ob/src/members.js
@@ -0,0 +1,58 @@
+{
+    members: [
+        {
+            name: 'Cody',
+            username: 'Parthene0n',
+            games: 'Call Of Duty (BLOPS 4, MW), Apex Legends',
+            likes: 'Food, learning photography/film making, spending time with my wife',
+            twitch: 'twitch.tv/parthene0n',
+            twitter: 'twitter.com/parthene0n',
+            ig: 'https://www.instagram.com/parthene0n'
+        },
+        {
+            name: 'Wilmer Valencia',
+            username: 'ReasonLatency',
+            games: 'Apex Legends, any other FPS/RPG',
+            likes: 'Photography, Cars, Music, Technology',
+            twitch: 'twitch.tv/reasonlatency',
+            twitter: 'twitter.com/reasonlatency',
+            ig: 'https://www.instagram.com/therealwilmer'
+        },
+        {
+            name: 'Ernest Bruno',
+            username: 'Nexticus',
+            games: 'Apex Legends, Call Of Duty, Anything',
+            likes: 'Food, video games, working out, music',
+            twitch: 'twitch.tv/nexticus',
+            twitter: 'twitter.com/nexticus',
+            ig: 'https://www.instagram.com/nexticus'
+        },
+        {
+            name: 'Lee',
+            username: 'Leesarous',
+            games: 'Overwatch, Apex Legends, Monster Hunter, Arcade Games',
+            likes: 'I like anime (not a weeaboo), I like Kpop (not a koreaboo), I like cats (and dogs), and I like my friends. UwU',
+            twitch: 'twitch.tv/leesarous',
+            twitter: 'twitter.com/leesarous',
+            ig: 'https://www.instagram.com/leesarous'
+        },
+        {
+            name: 'YassKiwii',
+            username: 'YassKiwii',
+            games: 'Mostly platformers and RPGs. Enjoys exploring different genres of games.',
+            likes: 'Food, all of it. Playing guitar, video games, and watching anime/tv',
+            twitch: 'twitch.tv/yasskiwii',
+            twitter: 'twitter.com/yasskiwii',
+            ig: 'https://www.instagram.com/yasskiwii'
+        },
+        {
+            name: 'Bad Wolf',
+            username: 'Bxdwolf1',
+            games: 'Apex Legends, League of Legends, Overwatch, CS: GO, Survival Games like Rust and DayZ.',
+            likes: 'I like hot wings. Outdoors and big tiddies. Also skateboarding fast eating ass and respecting weman. Hiking is also cool. Fave food is thicc booty. And burgers... Thicc burgers.',
+            twitch: 'twitch.tv/bxdwolf',
+            twitter: 'twitter.com/bxdwolf1',
+            ig: 'https://www.instagram.com/bxdwolf'
+        },
+    ]
+}
