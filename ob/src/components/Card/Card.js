05893a08fef75edd8680714f0b4f5ba4c7bd809e
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-12 09:58:44 -0800

    Display member info.

--- ob/src/components/Card/Card.js
+++ ob/src/components/Card/Card.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const Card = () => {
-    return (
-        <div>
+import './Card.css'
 
+const Card = (props) => {
+    return (
+        <div className='card'>
+            <div className='card-header'>
+                {props.member.username}
+            </div>
+            <div className='card-image'>
+                
+            </div>
+            <div className='card-body'>
+                
+            </div>
         </div>
     )
 };
