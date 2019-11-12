49a6e37226e7758c457fce0a4bdfc9cf96fb474b
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-12 09:57:56 -0800

    Import Card component and pass member data into card.

--- ob/src/components/Members/Members.js
+++ ob/src/components/Members/Members.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import {members} from '../../members';
 
+import Card from '../Card/Card';
+
 const Members = (props) => {
     return (
         <div>
             <main style={{marginTop: '70px', marginBottom: '74px'}}>
                 <h1>MEMBERS</h1>
                 <hr/>
-                {members.map(e => <h4>{e.name}</h4>)}
+                {members.map(e => <h4>{<Card member={e}/>}</h4>)}
             </main>
         </div>
     );
