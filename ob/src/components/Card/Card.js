3965c038c69b2e5a1f023748b4f53043ac349f41
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-12 14:05:35 -0800

    Resort to inline styling to ensure functionality of card flip.

--- ob/src/components/Card/Card.js
+++ ob/src/components/Card/Card.js
@@ -3,13 +3,41 @@ import React from 'react';
 import './Card.css'
 
 const Card = (props) => {
+    const [cardStatus, setCardStatus] = React.useState(false);
+
+    let cardFlip = () => {
+        setCardStatus(!cardStatus);
+    }
+
+    const styles = {
+        backgroundImage: `url(${props.member.img})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        zIndex: '-1000'
+    }
+
+    const cardStyle = {
+        width: '100%',
+        height: '100%',
+        position: 'relative',
+        transition: 'transform 1s',
+        transformStyle: 'preserve-3d',
+        cursor: 'pointer',
+        zIndex: '-1000'
+    }
+
+    const flipStyle = {...cardStyle,
+        transform: 'rotateY(180deg)',
+    }
+
+    let classToggle = (!cardStatus) ? cardStyle : flipStyle
+
     return (
-        <div className='card'>
-            <div className='card-inner'>     
-                <div className='card-front'>
-                    <img src={props.member.img}/>
-                </div>
-                <div className='card-back'>
+        <div className="scene">
+            <div onClick={cardFlip} style={classToggle}>
+                <div className="card__face card__face--front" style={styles}></div>
+                <div className="card__face card__face--back">
                     <div className='card-header'>
                         <p className='tag'>Twitch Username</p>
                         {props.member.username}
@@ -20,7 +48,7 @@ const Card = (props) => {
                     </div>
                     <div className='card-body'>
                         <p className='tag'>Likes</p>
-                        <textarea readonly>{props.member.likes}</textarea>
+                        <textarea readOnly value={props.member.likes}></textarea>
                     </div>
                 </div>
             </div>
