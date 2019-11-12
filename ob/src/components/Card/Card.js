9e436259125016a07468239e6e07eeb1d11cc505
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-12 11:24:26 -0800

    Start working on flipping card animation.

--- ob/src/components/Card/Card.js
+++ ob/src/components/Card/Card.js
@@ -5,24 +5,23 @@ import './Card.css'
 const Card = (props) => {
     return (
         <div className='card'>
-            <div className='card-front'>
-                <img src={props.member.img}/>
-            </div>
-            <div className='card-back'>
-                <div className='card-header'>
-                    <p className='tag'>Twitch Username</p>
-                    {props.member.username}
+            <div className='card-inner'>     
+                <div className='card-front'>
+                    <img src={props.member.img}/>
                 </div>
-                <div className='card-subtitle'>
-                    <p className='tag'>Name</p>
-                    {props.member.name}
-                </div>
-                <div className='card-image'>
-                    
-                </div>
-                <div className='card-body'>
-                    <p className='tag'>Likes</p>
-                    <textarea readonly>{props.member.likes}</textarea>
+                <div className='card-back'>
+                    <div className='card-header'>
+                        <p className='tag'>Twitch Username</p>
+                        {props.member.username}
+                    </div>
+                    <div className='card-subtitle'>
+                        <p className='tag'>Name</p>
+                        {props.member.name}
+                    </div>
+                    <div className='card-body'>
+                        <p className='tag'>Likes</p>
+                        <textarea readonly>{props.member.likes}</textarea>
+                    </div>
                 </div>
             </div>
         </div>
