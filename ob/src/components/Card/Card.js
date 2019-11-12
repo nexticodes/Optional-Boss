47ac04aff7ad7dcd53e8e17943632b6332706e92
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-12 10:35:53 -0800

    Create the HTML with the contents and proper tags for Card.

--- ob/src/components/Card/Card.js
+++ ob/src/components/Card/Card.js
@@ -6,13 +6,19 @@ const Card = (props) => {
     return (
         <div className='card'>
             <div className='card-header'>
+                <p className='tag'>Twitch Username</p>
                 {props.member.username}
+            </div>
+            <div className='card-subtitle'>
+                <p className='tag'>Name</p>
+                {props.member.name}
             </div>
             <div className='card-image'>
                 
             </div>
             <div className='card-body'>
-                
+                <p className='tag'>Likes</p>
+                <textarea>{props.member.likes}</textarea>
             </div>
         </div>
     )
