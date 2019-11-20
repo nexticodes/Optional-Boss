1a8a24bfa0bca02869a01987f358846afb5c0f92
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-11-20 12:25:47 -0800

    Add API calls, finally make the promises work.

--- ob/src/components/Twitch/Twitch.js
+++ ob/src/components/Twitch/Twitch.js
@@ -1,22 +1,41 @@
 import React, {Component} from 'react';
 import './Twitch.css';
 
-import members from './../../members';
+import axios from 'axios';
+import {members} from './../../members';
+
 
 class Twitch extends Component {
-    state = {
-
+    state= {
+        isLoading: true,
+        membersInfo: []
+    }
+    
+    async getMembersInfo(){
+        let promiseArray = await members.map((e) => axios.get(`https://api.twitch.tv/helix/streams?user_login=${e.username}`, {
+                headers: {
+                'Content-Type': 'application/json',
+                'Client-ID': 'bsmusanbzprjf19r25tet37rk3pe84'
+                }
+        }));
+        Promise.all(promiseArray).then((values) => this.setState({membersInfo:  values.map(e => e.data.data)}));
     };
 
-    render() {
-        console.log(members);
+
+    componentDidMount(){
+        this.getMembersInfo();
+    }
+
+    render(){
         return(
             <div>
                 <h1>Who's online?</h1>
                 <hr/>
+                {console.log(this.state.membersInfo)}
             </div>
-        )
+        ) 
     }
+
 }
 
 export default Twitch;
