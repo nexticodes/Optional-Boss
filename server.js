eb4456daf3503db0a7f2a2c9dcafcc7d58fac929
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-12-10 15:41:33 -0800

    Add twitter route into server.js... Add axios.

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const membersRoutes = require('./routes/members-route');
+const twitterRoutes = require('./routes/twitter-route');
 
 const app = express();
 
+app.use(cors());
+
 app.use('/api/members', membersRoutes);
+app.use('/api/twitter', twitterRoutes);
 
 
 
