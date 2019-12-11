6b6d5813d8b920aa7169af29773c72ebe1bf04fd
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-12-11 08:21:28 -0800

    Use Twit library to retrieve tweets. Could have done it vanilla, but I'm wasting too much time.

--- routes/twitter-route.js
+++ routes/twitter-route.js
@@ -1,37 +1,36 @@
 const express = require('express');
-const axios = require('axios')
+const axios = require('axios');
+const Twit = require('twit');
+let dotenv = require('dotenv').config();
 
 const router = express.Router();
 
-const getTweets = async(numQ) => {
-    let response = await axios.get(`https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=tmoptionalboss&count=${numQ}`, {
-        headers: {
-            'Host': 'api.twitter.com',
-            'Authorization': 'OAuth oauth_consumer_key="NIDpz1bJi7dNnoHxbeMrEvuJE",oauth_token="2965037396-0OnFzefjuU8LzN5mc3XXlSDWI3e29nk22kJpLpK",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1576019738",oauth_nonce="i5yMzVfM9dK",oauth_version="1.0",oauth_signature="388gn%2FNsUbkpVMc5GIH4x2qO4nw%3D"',
-            'cache-control': 'no-cache',
-        }
-    });
-    response.then((res) => res).catch((err) => err);
-}
+// const getTweets = async(numQ) => {
+//     let response = await axios.get(`https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=tmoptionalboss&count=${numQ}`, {
+//         headers: {
+//             'Authorization': `AuthStr`
+//         }
+//     });
+//     response.then((res) => res).catch((err) => err);
+// }
 
-let accessToken = '';
-
-const getToken = async() => {
-    let response = await axios.post(`https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=tmoptionalboss&count=${numQ}`, {
-        headers: {
-            'Host': 'api.twitter.com',
-            'Authorization': 'Basic NIDpz1bJi7dNnoHxbeMrEvuJE:01JbU89ltGQXK229SE9kkxE40QgfvcoqiuPyGhet4r4237wGec',
-        },
-        body: 'grant_type=client_credentials'
-    });
-}
-
+let T = new Twit({
+    consumer_key: process.env.API_KEY,
+    consumer_secret: process.env.API_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_SECRET
+});
 
 router.get('/:num', (req, res, next) => {
-    let numQ = req.params.num
-    let apiRes = getTweets(numQ);
-    getToken();
-    res.json(apiRes);
-})
+    T.get(`statuses/user_timeline.json?screen_name=tmoptionalboss&count=${router.params.num}`)
+        .catch((error) => {
+            res.json({
+                message: 'Something wrong happened',
+                error
+            })
+        }).then((data) => {
+            res.json(data);
+        });
+});
 
 module.exports = router;
