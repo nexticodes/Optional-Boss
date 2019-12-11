5b0183cd1bad0d3e691512cf666441a655d1e6eb
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-12-11 13:43:04 -0800

    Remove /build from .gitignore, pushing build.

---
+++ client/build/precache-manifest.fc0bfd542fa1b6fd1ed380f02680df24.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38bf11b725f9563ee9a20e131c2498fd",
+    "url": "/Optional-Boss/index.html"
+  },
+  {
+    "revision": "d5b51e6555a62f2c8049",
+    "url": "/Optional-Boss/static/css/main.a7fc6bfb.chunk.css"
+  },
+  {
+    "revision": "345db1a80b2efc538558",
+    "url": "/Optional-Boss/static/js/2.31a104e6.chunk.js"
+  },
+  {
+    "revision": "d5b51e6555a62f2c8049",
+    "url": "/Optional-Boss/static/js/main.d54e0c11.chunk.js"
+  },
+  {
+    "revision": "29b73e6f269ac3a5fca9",
+    "url": "/Optional-Boss/static/js/runtime-main.5efc760c.js"
+  },
+  {
+    "revision": "66380ffb6fb2cb0ef38b2d4b0e338bd6",
+    "url": "/Optional-Boss/static/media/OB logo.66380ffb.jpg"
+  },
+  {
+    "revision": "268c0ca64d0e4cb639e003236f214a5f",
+    "url": "/Optional-Boss/static/media/discord.268c0ca6.png"
+  },
+  {
+    "revision": "dc0ea39c7a39b6fa7e6bbcab2d2c1b63",
+    "url": "/Optional-Boss/static/media/live.dc0ea39c.png"
+  }
+]);
