776542e0de80c1dad5227ad3037d9e0a5630207b
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-12-11 12:11:00 -0800

    Add code to serve static files when build. Ready for deployment.

--- server.js
+++ server.js
@@ -11,11 +11,15 @@ const emailRoutes = require('./routes/email-route');
 const app = express();
 
 app.use(cors());
+app.use(express.static(path.join('client/build')))
 
 app.use('/api/members', membersRoutes);
 app.use('/api/twitter', twitterRoutes);
 app.use('/email', emailRoutes);
 
+app.use((req, res, next) => {
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
+})
 
 
 app.listen(5000);
