b59ae7727367d449cd2f656e6f8b864142cf839e
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-12-11 15:16:13 -0800

    Add social media links, but will need to disable css.

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 import './Card.css'
 
+import ig from '../../assets/Instagram-Logo-COLORED.png';
+import tw from '../../assets/Twitter-Logo-COLORED.png';
+import ttv from '../../assets/Twitch-Logo-COLORED.png';
+import dsc from '../../assets/Discord-Logo-COLORED.png';
+
 const Card = (props) => {
     const [cardStatus, setCardStatus] = React.useState(false);
 
@@ -45,6 +50,12 @@ const Card = (props) => {
                     <div className='card-body'>
                         <p className='tag'>Likes</p>
                         <textarea readOnly value={props.member.likes}></textarea>
+                    </div>
+                    <p className='tag'>Social Media</p>
+                    <div className='card-social'>
+                        <a href={props.member.ig} rel="noopener noreferrer"><img src={ig} alt='ig link'/></a>
+                        <a href={props.member.twitter} rel="noopener noreferrer"><img src={tw} alt='twitter link'/></a>
+                        <a href={props.member.twitch} rel="noopener noreferrer"><img src={ttv} alt='twitch link'/></a>
                     </div>
                 </div>
             </div>
