567a46a96a605067d38d16ef3542bd7c415d0944
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-12-12 11:27:09 -0800

    Change the onClick properties to only be triggered when image is clicked, not the whole card / scene itself.This change adds more effects to the social media buttons and adds a button that will flip the card back to its original place.

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -5,7 +5,6 @@ import './Card.css'
 import ig from '../../assets/Instagram-Logo-COLORED.png';
 import tw from '../../assets/Twitter-Logo-COLORED.png';
 import ttv from '../../assets/Twitch-Logo-COLORED.png';
-import dsc from '../../assets/Discord-Logo-COLORED.png';
 
 const Card = (props) => {
     const [cardStatus, setCardStatus] = React.useState(false);
@@ -28,7 +27,6 @@ const Card = (props) => {
         position: 'relative',
         transition: 'transform 1s',
         transformStyle: 'preserve-3d',
-        cursor: 'pointer',
         zIndex: '-1000'
     }
 
@@ -40,8 +38,8 @@ const Card = (props) => {
 
     return (
         <div className="scene">
-            <div onClick={cardFlip} style={classToggle}>
-                <div className="card__face card__face--front" style={styles}></div>
+            <div style={classToggle}>
+                <div onClick={cardFlip} className="card__face card__face--front" style={styles}></div>
                 <div className="card__face card__face--back">
                     <div className='card-header'>
                         <p className='tag'>Twitch Username</p>
@@ -53,10 +51,11 @@ const Card = (props) => {
                     </div>
                     <p className='tag'>Social Media</p>
                     <div className='card-social'>
-                        <a href={props.member.ig} rel="noopener noreferrer"><img src={ig} alt='ig link'/></a>
-                        <a href={props.member.twitter} rel="noopener noreferrer"><img src={tw} alt='twitter link'/></a>
-                        <a href={props.member.twitch} rel="noopener noreferrer"><img src={ttv} alt='twitch link'/></a>
+                        <a href={props.member.ig} rel="noopener noreferrer" target="_blank"><img src={ig} alt='ig link'/></a>
+                        <a href={props.member.twitter} rel="noopener noreferrer" target="_blank"><img src={tw} alt='twitter link'/></a>
+                        <a href={props.member.twitch} rel="noopener noreferrer" target="_blank"><img src={ttv} alt='twitch link'/></a>
                     </div>
+                    <button onClick={cardFlip}>Flip back</button>
                 </div>
             </div>
         </div>
