2b3926452174564ddffb22d95bc588ef34d2c427
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-12-12 10:52:38 -0800

    Add appropriate links.

--- client/src/members.js
+++ client/src/members.js
@@ -6,9 +6,9 @@ module.exports = {
             username: 'Parthene0n',
             games: 'Call Of Duty (BLOPS 4, MW), Apex Legends',
             likes: 'Food, learning photography/film making, spending time with my wife',
-            twitch: 'twitch.tv/parthene0n',
-            twitter: 'twitter.com/parthene0n',
-            ig: 'instagram.com/parthene0n',
+            twitch: 'https://twitch.tv/parthene0n',
+            twitter: 'https://twitter.com/parthene0n',
+            ig: 'https://instagram.com/parthene0n',
             status: null
         },
         {
@@ -17,9 +17,9 @@ module.exports = {
             username: 'ReasonLatency',
             games: 'Apex Legends, any other FPS/RPG',
             likes: 'Photography, Cars, Music, Technology',
-            twitch: 'twitch.tv/reasonlatency',
-            twitter: 'twitter.com/reasonlatency',
-            ig: 'instagram.com/therealwilmer',
+            twitch: 'https://twitch.tv/reasonlatency',
+            twitter: 'https://twitter.com/reasonlatency',
+            ig: 'https://instagram.com/therealwilmer',
             status: null
         },
         {
@@ -28,9 +28,9 @@ module.exports = {
             username: 'Nexticus',
             games: 'Apex Legends, Call Of Duty, Anything',
             likes: 'Food, video games, working out, music, coding, spending time with my girl.',
-            twitch: 'twitch.tv/nexticus',
-            twitter: 'twitter.com/nexticus',
-            ig: 'instagram.com/nexticus',
+            twitch: 'https://twitch.tv/nexticus',
+            twitter: 'https://twitter.com/nexticus',
+            ig: 'https://instagram.com/nexticus',
             status: null
         },
         {
@@ -39,9 +39,9 @@ module.exports = {
             username: 'Leesarous',
             games: 'Overwatch, Apex Legends, Monster Hunter, Arcade Games',
             likes: 'I like anime (not a weeaboo), I like Kpop (not a koreaboo), I like cats (and dogs), and I like my friends. UwU',
-            twitch: 'twitch.tv/leesarous',
-            twitter: 'twitter.com/leesarous',
-            ig: 'instagram.com/leesarous',
+            twitch: 'https://twitch.tv/leesarous',
+            twitter: 'https://twitter.com/leesarous',
+            ig: 'https://instagram.com/leesarous',
             status: null
         },
         {
@@ -50,9 +50,9 @@ module.exports = {
             username: 'yasskiwii',
             games: 'Mostly platformers and RPGs. Enjoys exploring different genres of games.',
             likes: 'Food, all of it. Playing guitar, video games, and watching anime/tv',
-            twitch: 'twitch.tv/yasskiwii',
-            twitter: 'twitter.com/yasskiwii',
-            ig: 'instagram.com/yasskiwii',
+            twitch: 'https://twitch.tv/yasskiwii',
+            twitter: 'https://twitter.com/yasskiwii',
+            ig: 'https://instagram.com/yasskiwii',
             status: null
         },
         {
@@ -61,9 +61,9 @@ module.exports = {
             username: 'bxdwolf',
             games: 'Apex Legends, League of Legends, Overwatch, CS: GO, Survival Games like Rust and DayZ.',
             likes: 'I like hot wings. Outdoors and big tiddies. Also skateboarding fast eating ass and respecting weman. Hiking is also cool. Fave food is thicc booty. And burgers... Thicc burgers.',
-            twitch: 'twitch.tv/bxdwolf',
-            twitter: 'twitter.com/bxdwolf1',
-            ig: 'instagram.com/bxdwolf',
+            twitch: 'https://twitch.tv/bxdwolf',
+            twitter: 'https://twitter.com/bxdwolf1',
+            ig: 'https://instagram.com/bxdwolf',
             status: null
         },
     ]
